bc59e28795876c12085ab18131ec2864e0b56ae4
Reetesh Kumar <reetesh043@gmail.com>
2023-09-10 23:28:18 +0100

    liquibase example with spring boot

---
+++ src/main/java/com/example/reet/liquibase/domain/TestUser.java
@@ -0,0 +1,35 @@
+package com.example.reet.liquibase.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "test_user")
+public class TestUser {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
---
+++ src/test/java/com/example/reet/liquibase/SpringBootLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reet.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
